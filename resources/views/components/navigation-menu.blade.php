9c3814f4fd77d65ffd3a6e651f2d6177b6afcd84
xfathurrahman <xxxfathurrahman@gmail.com>
2021-02-20 18:57:19 +0700

    done apply sidebar , navbar , toggle profile dari template stisla.

---
+++ resources/views/components/navigation-menu.blade.php
@@ -0,0 +1,262 @@
+@php
+    $user = auth()->user();
+@endphp
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg main-navbar">
+    <div class="section-header">
+        @isset($header_content)
+            {{ $header_content }}
+        @else
+            {{ __('Halaman') }}
+        @endisset
+    </div>
+    <form class="form-inline mr-auto">
+        <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+        </ul>
+        <div class="search-element">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
+            <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+            <div class="search-backdrop"></div>
+            <div class="search-result">
+                <div class="search-header">
+                    Histories
+                </div>
+                <div class="search-item">
+                    <a href="#">How to hack NASA using CSS</a>
+                    <a href="#" class="search-close"><i class="fas fa-times"></i></a>
+                </div>
+                <div class="search-item">
+                    <a href="#">Kodinger.com</a>
+                    <a href="#" class="search-close"><i class="fas fa-times"></i></a>
+                </div>
+                <div class="search-item">
+                    <a href="#">#Stisla</a>
+                    <a href="#" class="search-close"><i class="fas fa-times"></i></a>
+                </div>
+                <div class="search-header">
+                    Result
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <img class="mr-3 rounded" width="30" src="{{asset('stisla/img/products/product-3-50.png')}}" alt="product">
+                        oPhone S9 Limited Edition
+                    </a>
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <img class="mr-3 rounded" width="30" src="{{asset('stisla/img/products/product-2-50.png')}}" alt="product">
+                        Drone X2 New Gen-7
+                    </a>
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <img class="mr-3 rounded" width="30" src="{{asset('stisla/img/products/product-1-50.png')}}" alt="product">
+                        Headphone Blitz
+                    </a>
+                </div>
+                <div class="search-header">
+                    Projects
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <div class="search-icon bg-danger text-white mr-3">
+                            <i class="fas fa-code"></i>
+                        </div>
+                        Stisla Admin Template
+                    </a>
+                </div>
+                <div class="search-item">
+                    <a href="#">
+                        <div class="search-icon bg-primary text-white mr-3">
+                            <i class="fas fa-laptop"></i>
+                        </div>
+                        Create a new Homepage Design
+                    </a>
+                </div>
+            </div>
+        </div>
+    </form>
+    <ul class="navbar-nav navbar-right">
+        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+                <div class="dropdown-header">Messages
+                </div>
+                <div class="dropdown-list-content dropdown-list-message">
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="{{asset('stisla/img/avatar/avatar-1.png')}}" class="rounded-circle">
+                            <div class="is-online"></div>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Kusnaedi</b>
+                            <p>Hello, Bro!</p>
+                            <div class="time">10 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="{{asset('stisla/img/avatar/avatar-2.png')}}" class="rounded-circle">
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Dedik Sugiharto</b>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                            <div class="time">12 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="{{asset('stisla/img/avatar/avatar-3.png')}}" class="rounded-circle">
+                            <div class="is-online"></div>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Agung Ardiansyah</b>
+                            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <div class="time">12 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="{{asset('stisla/img/avatar/avatar-4.png')}}" class="rounded-circle">
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Ardian Rahardiansyah</b>
+                            <p>Duis aute irure dolor in reprehenderit in voluptate velit ess</p>
+                            <div class="time">16 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-avatar">
+                            <img alt="image" src="{{asset('stisla/img/avatar/avatar-5.png')}}" class="rounded-circle">
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Alfa Zulkarnain</b>
+                            <p>Exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                            <div class="time">Yesterday</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="dropdown-footer text-center">
+                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                </div>
+            </div>
+        </li>
+        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+                <div class="dropdown-header">Notifications</div>
+                <div class="dropdown-list-content dropdown-list-icons">
+                    <a href="#" class="dropdown-item dropdown-item-unread">
+                        <div class="dropdown-item-icon bg-primary text-white">
+                            <i class="fas fa-code"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            Template update is available now!
+                            <div class="time text-primary">2 Min Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-info text-white">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>You</b> and <b>Dedik Sugiharto</b> are now friends
+                            <div class="time">10 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-success text-white">
+                            <i class="fas fa-check"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            <b>Kusnaedi</b> has moved task <b>Fix bug header</b> to <b>Done</b>
+                            <div class="time">12 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-danger text-white">
+                            <i class="fas fa-exclamation-triangle"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            Low disk space. Let's clean it!
+                            <div class="time">17 Hours Ago</div>
+                        </div>
+                    </a>
+                    <a href="#" class="dropdown-item">
+                        <div class="dropdown-item-icon bg-info text-white">
+                            <i class="fas fa-bell"></i>
+                        </div>
+                        <div class="dropdown-item-desc">
+                            Welcome to Stisla template!
+                            <div class="time">Yesterday</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="dropdown-footer text-center">
+                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
+                </div>
+            </div>
+        </li>
+        <li class="dropdown">
+            <!-- Profile -->
+            <div class="ml-3 relative">
+                <x-jet-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                            </button>
+                        @else
+                            <span class="inline-flex rounded-md">
+                              <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                  {{ Auth::user()->name }}
+                                  <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                      <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                  </svg>
+                              </button>
+                        </span>
+                        @endif
+                    </x-slot>
+
+                    <x-slot name="content">
+
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            Halo ,
+                            <div class="text-dark text-lg font-bold font-thin">
+                                {{ Auth::user()->name }}
+                            </div>
+                        </div>
+
+                        <div class="border-t border-gray-100"></div>
+
+                        <div class="text-dark">
+                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                <i class="far fa-user fa-bold">&emsp;{{ __('Profile') }}</i>
+                            </x-jet-dropdown-link>
+                        </div>
+
+                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                            <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                                <i class="fas fa-fire">&emsp;{{ __('API Tokens') }}</i>
+                            </x-jet-dropdown-link>
+                        @endif
+                        <div class="border-t border-gray-100"></div>
+
+                        <!-- Authentication -->
+                        <div class="text-danger-all">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-jet-dropdown-link href="{{ route('logout') }}"
+                                                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    <i class="fas fa-sign-out-alt">&emsp;{{ __('Logout') }}</i>
+                                </x-jet-dropdown-link>
+                            </form>
+                        </div>
+                    </x-slot>
+                </x-jet-dropdown>
+            </div>
+        </li>
+    </ul>
+</nav>
